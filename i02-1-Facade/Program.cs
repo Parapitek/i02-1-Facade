aadd48f942c13f3a7962a9c630d80501cff4e09d
Rustam <safarov.rust@gmail.com>
2023-02-18 10:58:26 +0700

    Добавьте файлы проекта.

---
+++ i02-1-Facade/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace i02_1_Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
